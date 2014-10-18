6f9d7b93cf476cbba8047045f97eefbfdb938c91
Hipertextos <hipertextos@gmail.com>
2014-10-18 17:20:42 +0200

    Analytics

--- src/js/ga.js
+++ src/js/ga.js
@@ -4,6 +4,6 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-6785011-10', 'auto');
+  ga('create', 'UA-20427438-1', 'auto');
   ga('send', 'pageview');
 
