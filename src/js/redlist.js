d6845ccd50dc503b4a88641e3094fd5e99fe318f
Hipertextos <hipertextos@gmail.com>
2014-11-15 17:40:23 +0100

    Redlist

--- src/js/redlist.js
+++ src/js/redlist.js
@@ -39,7 +39,7 @@ $(document).ready(function () {
 	});
 	$('#feed-bloc-campanyes').rssfeed('http://bloc.comunistes.cat/feeds/posts/default/-/Campanyes',{
 		snippet: false,
-		limit: 2
+		limit: 3
 	});	
 	
 	// Feeds filtrats de imatges
